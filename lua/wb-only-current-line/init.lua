7580a58ee6b9df554e274ac7fc64b077cd3fb0f4
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-05-19 22:01:57 +0900

    Fix visual mode

--- lua/wb-only-current-line/init.lua
+++ lua/wb-only-current-line/init.lua
@@ -1,13 +1,9 @@
 local M = {}
 
-local function motion(mode, key, back_key)
+local function motion(key, back_key)
 	if vim.v.count1 > 1 then
 		vim.cmd("normal! " .. vim.v.count1 .. key)
 		return
-	end
-
-	if mode == "v" then
-		vim.cmd([[normal! gv]])
 	end
 
 	local initial_line = vim.fn.line(".")
@@ -21,7 +17,7 @@ end
 
 function M.set_keymap(mode, key, back_key)
 	vim.keymap.set(mode, key, function()
-		motion(mode, key, back_key)
+		motion(key, back_key)
 	end, { noremap = true, expr = false, silent = false })
 end
 
